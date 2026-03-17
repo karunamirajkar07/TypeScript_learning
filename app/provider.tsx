484243ef8d4ed6db71de4c176308b9f35d574997
karuna mirajkar <kmirajkar@hexalytics.com>
2026-03-17 11:35:42 +0530

    check devtools

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,14 +2,18 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactNode, useState } from "react"
+import { ReactQueryDevtools ,ReactQueryDevtoolsPanel} from "@tanstack/react-query-devtools"
 
 export default function Providers({ children }: { children: ReactNode }) {
 
   const [queryClient] = useState(() => new QueryClient())
+  const [isOpen , setIsOpen ] = useState(false)
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+       {/* The rest of your application */}
+       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
--- app/react-Query/page.tsx
+++ app/react-Query/page.tsx
@@ -1,33 +1,36 @@
 "use client"
 import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import React from 'react'
 
 export default function ReactQuery() {
 
     interface Product {
         id: number
-        title : string
-        category : string
+        title: string
+        category: string
     }
+
+    const fetchData = async (): Promise<Product[]> => {
+        const data = await axios.get("https://6776460912a55a9a7d0b1821.mockapi.io/product")
+        return data.data
+        }
 
     const { data } = useQuery<Product[]>({
         queryKey: ["product"],
-        queryFn: async () => {
-            const response = await fetch("https://6776460912a55a9a7d0b1821.mockapi.io/product")
-            return await response.json()
-        }
+        queryFn: fetchData
     })
-    return (
-        <div>
-            <ul>
-                {data?.map((x) => (
-                    <div>
+return (
+    <div>
+        <ul>
+            {data?.map((x) => (
+                <div>
 
-                        <li key={x.id}>{x.title}</li>
-                        <li key={x.category}>{x.category}</li>
-                    </div>
-                ))}
-            </ul>
-        </div>
-    )
+                    <li key={x.id}>{x.title}</li>
+                    <li key={x.category}>{x.category}</li>
+                </div>
+            ))}
+        </ul>
+    </div>
+)
 }
